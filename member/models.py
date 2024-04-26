4483995a79f24a8b008296e573a47fb768ebd9ab
haunpapa <good78912@gmail.com>
2024-04-26 18:33:13 +0900

    Feat : Email API & Common Model Setting

--- member/models.py
+++ member/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
+
+class Member(CommonModel):
+    member_email = models.EmailField(max_length=100, unique=True)
+    member_password = models.CharField(max_length=100)
+    auth_group = models.CharField(max_length=20)
+    is_admin = models.BooleanField(default=False)
+    mem_waiting = models.BooleanField(default=False)
+    mem_number = models.CharField(max_length=20)
